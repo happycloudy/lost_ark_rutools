0f659cee88d644929dd55349ecab6f2128b7a053
happycloudy <arion.mikola@gmail.com>
2023-09-12 00:10:45 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navigation from "./components/Navigation/Navigation.tsx";
-import AurelTrip from "./components/modules/AurelTrip/components/AurelTrip/AurelTrip.tsx";
-import WhereStay from "./components/modules/WhereStay/components/WhereStay/WhereStay.tsx";
+import AurelTrip from "./modules/modules/AurelTrip/components/AurelTrip/AurelTrip.tsx";
+import WhereStay from "./modules/modules/WhereStay/components/WhereStay/WhereStay.tsx";
 
 function App() {
 
