120a0a3153278cc3ecf6e769032c83d47fa7eda3
happycloudy <arion.mikola@gmail.com>
2023-09-12 01:18:53 +0300

    add footer link open new page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <a href={'https://github.com/happycloudy/lost_ark_rutools'} className={styles.link}>
+            <a href={'https://github.com/happycloudy/lost_ark_rutools'} target={'#blank'} className={styles.link}>
                 Github
             </a>
         </footer>
